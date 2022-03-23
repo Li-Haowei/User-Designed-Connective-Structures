0ba2f1560617850ee06f09091d579a4cf78083d8
Li-Haowei <haoweilee666@gmail.com>
2022-03-23 12:57:05 -0400

    project imported from local computer

---
+++ README.md
@@ -0,0 +1,2 @@
+# cs581-curve-fabrication
+Our idea is to have a software such that takes in user input and output a curve for marble track
---
+++ Turn2D3D/writeDFX.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 23 12:34:20 2022
+
+@author: Haowei Li
+"""
+
+from dxfwrite import DXFEngine as dxf
