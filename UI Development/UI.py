da3e18eae07700d5bf850617be96a5aaecf0df68
Li-Haowei <haoweilee666@gmail.com>
2022-03-30 18:31:53 -0400

    Update UI.py

--- UI Development/UI.py
+++ UI Development/UI.py
@@ -8,13 +8,17 @@ import os
 import pygame
 import tkinter as tk
 from tkinter import *
+import platform
 
+
+screenOn = True
 root = tk.Tk()
 embed = tk.Frame(root, width = 500, height = 500) #creates embed frame for pygame window
 embed.grid(columnspan = (600), rowspan = 500) # Adds grid
 embed.pack(side = LEFT) #packs window to the left
 buttonwin = tk.Frame(root, width = 75, height = 500)
 buttonwin.pack(side = LEFT)
+#Button(root, text="Quit", command=root.destroy).pack()
 os.environ['SDL_WINDOWID'] = str(embed.winfo_id())
 os.environ['SDL_VIDEODRIVER'] = 'windib'
 screen = pygame.display.set_mode((500,500))
@@ -30,6 +34,16 @@ button1 = Button(buttonwin,text = 'Draw',  command=draw)
 button1.pack(side=LEFT)
 root.update()
 
-while True:
+done = False
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                 done = True
     pygame.display.update()
     root.update()
+    if done:
+        pygame.quit()
+        root.destroy()
